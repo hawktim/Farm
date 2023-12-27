fdc2ba1c01b85415a55e789d6778a6bfc0a45d71
hawktim <hawktim@users.noreply.github.com>
2023-12-27 16:15:48 +0300

    Чистка кода

--- Program.cs
+++ Program.cs
@@ -12,34 +12,4 @@ namespace FarmConsoleApp
             defaultController.GetActions(new FEventArgs { TypeAction = TypeAction.List });
         }
     }
-
-    public class Product
-    {
-        public int IdProduct { get; set; }
-        public string NameProduct { get; set; }
-    }
-
-    public class Aptek
-    {
-        public string IdAptek { get; set; }
-        public string NameAptek { get; set; }
-        public string Address { get; set; }
-        public string Phone { get; set; }
-    }
-
-    public class Warehouse
-    {
-        public int IdWarehouse { get; set; }
-        public int IdAptek { get; set; }
-        public string NameWarehouse { get; set; }
-    }
-
-    public class Partition
-    {
-        public int IdPartition { get; set; }
-        public int IdProduct { get; set; }
-        public int IdWarehouse { get; set; }
-        public decimal Quantity { get; set; }
-    }
-
 }
--- TypeAction.cs
+++ TypeAction.cs
@@ -2,12 +2,11 @@
 {
     public enum TypeAction
     {
-        List=0,
-        Add=1,
-        Remove=2,
-        Select=3,
-        Detail=4,
-        Othes=5
+        List = 0,
+        Add = 1,
+        Remove = 2,
+        Select = 3,
+        Detail = 4,
+        Othes = 5
     }
-
 }
